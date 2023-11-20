e7daa1bc08f69796dfc0d23888acc1fc660efcc1
Aleesha Naxakis <aleesha-@hotmail.com>
2023-11-20 23:20:14 +1100

    adding router tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,33 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+      // be sure to include its associated Product data
+    const tagData = await Tag.findAll( {
+      include: [{ model: Product}]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status (500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findbyPk(req.params.id, {
+  // be sure to include its associated Product data     
+      include: [{ model: Product }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return
+    }
+  }
+
 });
 
 router.post('/', (req, res) => {
