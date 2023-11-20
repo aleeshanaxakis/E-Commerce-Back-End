ce9511e9c63c83f1c44f8b1e68da724e75bcf018
Aleesha Naxakis <aleesha-@hotmail.com>
2023-11-20 13:34:48 +1100

    created relationships and associations

--- models/index.js
+++ models/index.js
@@ -1,16 +1,36 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('./product');
+const Category = require('./category');
+const Tag = require('./tag');
+const ProductTag = require('./productTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongstoMany(Tag, {
+    through: {
+        model: ProductTag,
+        unique: false
+    },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongstoMany(Product, {
+    through: {
+        model: ProductTag,
+        unique: false
+    },
+});
 
 module.exports = {
   Product,
